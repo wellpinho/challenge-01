d205905794f27ead5a37d58b825d257c2557774d
wellpinho <wellpinho@outlook.com>
2021-08-25 20:59:59 -0300

    styled navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,32 +1,32 @@
 <template>
-  <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light">
-      <a class="navbar-brand" href="#">spense.</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <div class="container">
+        <a class="navbar-brand" href="#">spense.</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="#">{{menus[0]}}</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">{{ menus[1]}}</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">{{menus[2]}}</a>
-          </li>
-        </ul>
-        <span class="navbar-nav">
-          <li class="nav-item">
-              <a class="nav-link" href="#">{{menus[3]}}</a>
-          </li>
-        </span>
-        <button class="btn btn-dark my-2 my-sm-0" type="submit">Get Started</button>
+        <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">{{menus[0]}}</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">{{ menus[1]}}</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">{{menus[2]}}</a>
+            </li>
+          </ul>
+          <span class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">{{menus[3]}}</a>
+            </li>
+          </span>
+          <button class="btn btn-dark my-2 my-sm-0" type="submit">Get Started</button>
+        </div>
       </div>
     </nav>
-  </div>
 </template>
 
 <script>
@@ -52,6 +52,13 @@ export default {
 <style scoped>
   .mr-auto, .mx-auto {
     margin-left: auto;
+  }
+
+  .navbar {
+    position: fixed;
+    width: 100%;
+    z-index: 9998;
+    background-color: #E9ECEF;
   }
   .navbar-light .navbar-nav .nav-link {
     color: rgba(0, 0, 0, .9);
